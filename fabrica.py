b0b8b53319818db2e4a2418df5fccf58e3cf08f0
Robinson <robinquiroz57@gmail.com>
2025-04-01 19:04:32 -0500

    Feat se crea main para app de fabrica corona

---
+++ fabrica.py
@@ -0,0 +1,12 @@
+# Una fabrica de productos ceramicos
+# Elabora 10 productos para CORONA
+# Cada producto esta identidicado por:
+# id, referencia (8 caracteres), descripción,
+# precio de fabricacion individual,
+# numero de piezas fabricadas,
+# numero de piezas defectuosas
+# fecha de envio de las piezas
+
+# construya una función para agregar piezas la pedido
+# construya una funcion para ver el pedido
+

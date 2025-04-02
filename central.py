9c2c335b4c7a245781435599feb7bfa181d923b8
Robinson <robinquiroz57@gmail.com>
2025-04-01 20:44:16 -0500

    feat se hace llamado a la funciones en el archivo central

--- central.py
+++ central.py
@@ -1,9 +1,15 @@
+from fabrica import crearPieza, agregarPieza, mostrarPedido
+
 lista=[]
 
 #Pedir datos por teclado
+id=int(input("Digita el id: "))
 
 #Llamar a la funcion que crea la pieza
+pieza=crearPieza(id, "abcd001", "plato maravilloso", 50000, 100, 10, "1/4/2025")
 
 #Llamar a la funcion que agrega la pieza a la lista
+agregarPieza(lista, pieza)
 
 #Llamar a la funcion que muestra la lista
+mostrarPedido(lista)
